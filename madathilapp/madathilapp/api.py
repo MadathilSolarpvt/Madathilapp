07064f09851530e6d2f2575c8e9e6e0583cba098
SouravMadathil <madathilsoftware.com>
2026-08-06 12:35:09 +0000

    updated api file role and quotation details

--- madathilapp/madathilapp/api.py
+++ madathilapp/madathilapp/api.py
@@ -1,5 +1,6 @@
 import frappe
 
+
 @frappe.whitelist()
 def get_quotation_manager():
     user = frappe.db.sql("""
@@ -15,6 +16,49 @@ def get_quotation_manager():
     """, as_dict=True)
 
     return user[0] if user else {}
+
+
+@frappe.whitelist()
+def get_my_quotations():
+
+    user = frappe.session.user
+
+    quotations = frappe.get_all(
+        "Quotation",
+        filters={
+            "custom_sales_person": user
+        },
+        fields=[
+            "name",
+            "customer_name",
+            "transaction_date",
+            "valid_till",
+            "status",
+            "grand_total",
+            "company"
+        ],
+        order_by="modified desc"
+    )
+
+    for quotation in quotations:
+
+        file = frappe.get_all(
+            "File",
+            filters={
+                "attached_to_doctype": "Quotation",
+                "attached_to_name": quotation["name"]
+            },
+            fields=[
+                "file_name",
+                "file_url",
+                "is_private"
+            ],
+            limit=1
+        )
+
+        quotation["attachment"] = file[0] if file else {}
+
+    return quotations
 
 
 @frappe.whitelist()
